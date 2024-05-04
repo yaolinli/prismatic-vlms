6ee061e40462b81fe11c4fc2ff7c246cc14ac9c6
yaolinli <2016202236@ruc.edu.cn>
2024-05-04 17:44:12 +0800

    1) set wandb key; 2)add --run_root_dir in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 # gpu settings
 gpu_id=0,1
 gpu_num=2
+run_root_dir='runs'
 
 # test code
 model_name='phi2'
@@ -8,14 +9,14 @@ vision_backbone="dinosiglip-vit-so-384px"
 resize_strategy='resize-naive'
 projector='avgpool2_144'
 
-WANDB_MODE=offline \
+# WANDB_MODE=offline \
 CUDA_VISIBLE_DEVICES=$gpu_id torchrun --standalone --nnodes 1 --nproc-per-node $gpu_num scripts/pretrain.py \
   --model.type "reproduction-llava-v15+7b" \
   --model.model_id "one-stage_"${model_name} \
   --model.vision_backbone_id $vision_backbone \
   --model.image_resize_strategy $resize_strategy \
   --model.arch_specifier 'no-align+'$projector \
-  --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${projector} --model.finetune_per_device_batch_size 2
+  --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${projector} --model.finetune_per_device_batch_size 2 --run_root_dir $run_root_dir
 
 
 #####################################
@@ -31,13 +32,13 @@ for vision_backbone in "${vision_backbones[@]}"
 do
     for projector in "${projectors[@]}"
     do
-        WANDB_MODE=offline \
+        # WANDB_MODE=offline \
         CUDA_VISIBLE_DEVICES=$gpu_id torchrun --standalone --nnodes 1 --nproc-per-node $gpu_num scripts/pretrain.py \
         --model.type "reproduction-llava-v15+7b" \
         --model.model_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} \
         --model.vision_backbone_id $vision_backbone \
         --model.image_resize_strategy $resize_strategy \
-        --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} --model.arch_specifier 'no-align+'$projector --model.finetune_per_device_batch_size 2
+        --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} --model.arch_specifier 'no-align+'$projector --model.finetune_per_device_batch_size 2 --run_root_dir $run_root_dir
     done
 done
 
@@ -55,13 +56,13 @@ vision_backbone="clip-vit-l-336px"
 resize_strategy='resize-naive'
 projector='gelu-mlp'
 
-WANDB_MODE=offline \
+# WANDB_MODE=offline \
 CUDA_VISIBLE_DEVICES=$gpu_id torchrun --standalone --nnodes 1 --nproc-per-node $gpu_num scripts/pretrain.py \
   --model.type "reproduction-llava-v15+7b" \
   --model.model_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} \
   --model.vision_backbone_id $vision_backbone \
   --model.image_resize_strategy $resize_strategy \
-  --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} --model.arch_specifier 'no-align+'$projector --model.finetune_per_device_batch_size 1
+  --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} --model.arch_specifier 'no-align+'$projector --model.finetune_per_device_batch_size 1 --run_root_dir $run_root_dir
 
 # e2: clip-vit-l-336px + vicuna-v15-7b + avgpool2_144
 model_name="vicuna-v15-7b"
@@ -69,13 +70,13 @@ vision_backbone="clip-vit-l-336px"
 resize_strategy='resize-naive'
 projector='avgpool2_144'
 
-WANDB_MODE=offline \
+# WANDB_MODE=offline \
 CUDA_VISIBLE_DEVICES=$gpu_id torchrun --standalone --nnodes 1 --nproc-per-node $gpu_num scripts/pretrain.py \
   --model.type "reproduction-llava-v15+7b" \
   --model.model_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} \
   --model.vision_backbone_id $vision_backbone \
   --model.image_resize_strategy $resize_strategy \
-  --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} --model.arch_specifier 'no-align+'$projector --model.finetune_per_device_batch_size 1
+  --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} --model.arch_specifier 'no-align+'$projector --model.finetune_per_device_batch_size 1 --run_root_dir $run_root_dir
 
 # e3: dinosiglip-vit-so-384px + vicuna-v15-7b + avgpool2_144
 model_name="vicuna-v15-7b"
@@ -83,13 +84,13 @@ vision_backbone="dinosiglip-vit-so-384px"
 resize_strategy='resize-naive'
 projector='avgpool2_144'
 
-WANDB_MODE=offline \
+# WANDB_MODE=offline \
 CUDA_VISIBLE_DEVICES=$gpu_id torchrun --standalone --nnodes 1 --nproc-per-node $gpu_num scripts/pretrain.py \
   --model.type "reproduction-llava-v15+7b" \
   --model.model_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} \
   --model.vision_backbone_id $vision_backbone \
   --model.image_resize_strategy $resize_strategy \
-  --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} --model.arch_specifier 'no-align+'$projector --model.finetune_per_device_batch_size 1
+  --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} --model.arch_specifier 'no-align+'$projector --model.finetune_per_device_batch_size 1 --run_root_dir $run_root_dir
 
 # e4: dinosiglip-vit-so-384px + vicuna-v15-7b + cabstractor2_144
 model_name="vicuna-v15-7b"
@@ -97,11 +98,10 @@ vision_backbone="dinosiglip-vit-so-384px"
 resize_strategy='resize-naive'
 projector='cabstractor_144'
 
-WANDB_MODE=offline \
+# WANDB_MODE=offline \
 CUDA_VISIBLE_DEVICES=$gpu_id torchrun --standalone --nnodes 1 --nproc-per-node $gpu_num scripts/pretrain.py \
   --model.type "reproduction-llava-v15+7b" \
   --model.model_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} \
   --model.vision_backbone_id $vision_backbone \
   --model.image_resize_strategy $resize_strategy \
-  --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} --model.arch_specifier 'no-align+'$projector --model.finetune_per_device_batch_size 1
-
+  --model.llm_backbone_id $model_name  --run_id "one-stage_"${model_name}"+"${vision_backbone}"+"${projector} --model.arch_specifier 'no-align+'$projector --model.finetune_per_device_batch_size 1 --run_root_dir $run_root_dir
